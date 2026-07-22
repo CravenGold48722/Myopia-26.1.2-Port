bb295cc7ee82318115a3362cc92adec188aa4505
CravenGold48722 <cravengold48722@gmail.com>
2026-07-22 00:19:22 -0700

    Initial commit adding stuff

---
+++ src/main/java/mba/sno/myopia/client/MyopiaClient.java
@@ -0,0 +1,45 @@
+package mba.sno.myopia.client;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.Identifier;
+import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyopiaClient implements ClientModInitializer {
+    public static final String MOD_ID = "Myopia";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    // Use Identifier.fromNamespaceAndPath in MC 26.1+
+    public static final KeyMapping.Category MYOPIA_CATEGORY = KeyMapping.Category.register(
+            Identifier.fromNamespaceAndPath("myopia", "main")
+    );
+
+    private static final String KEY_TOGGLE_MYOPIA = "key.myopia.toggle";
+    public static boolean enableBlur = true;
+
+    private static KeyMapping toggleKey;
+
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("Making Minecraft look Bad™ since 2024!");
+
+        toggleKey = KeyMappingHelper.registerKeyMapping(new KeyMapping(
+                KEY_TOGGLE_MYOPIA, // Translation key for the keybind name
+                InputConstants.Type.KEYSYM, // Key type (keyboard)
+                GLFW.GLFW_KEY_O, // Default key
+                MYOPIA_CATEGORY // Category object
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (toggleKey.consumeClick()) {
+                enableBlur = !enableBlur;
+                client.levelRenderer.allChanged();
+            }
+        });
+    }
+}
---
+++ src/main/java/mba/sno/myopia/client/mixin/BlurMixin.java
@@ -0,0 +1,19 @@
+package mba.sno.myopia.client.mixin;
+
+import com.mojang.blaze3d.textures.FilterMode;
+import mba.sno.myopia.client.MyopiaClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(targets = "com.mojang.blaze3d.textures.GpuTexture")
+public class BlurMixin {
+    @ModifyVariable(at = @At(value = "HEAD"), method = "setTextureFilter(Lcom/mojang/blaze3d/textures/FilterMode;Lcom/mojang/blaze3d/textures/FilterMode;Z)V", ordinal = 0, argsOnly = true)
+    private static FilterMode setMin(FilterMode value) {
+        return MyopiaClient.enableBlur ? FilterMode.LINEAR : value;
+    }
+    @ModifyVariable(at = @At(value = "HEAD"), method = "setTextureFilter(Lcom/mojang/blaze3d/textures/FilterMode;Lcom/mojang/blaze3d/textures/FilterMode;Z)V", ordinal = 1, argsOnly = true)
+    private static FilterMode setMag(FilterMode value) {
+        return MyopiaClient.enableBlur ? FilterMode.LINEAR : value;
+    }
+}
